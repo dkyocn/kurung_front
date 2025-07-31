83a85b0acddb9f25058d994a1decd8adea57e584
Geonho0719 <kknh97@naver.com>
2025-08-01 00:12:31 +0900

    [FIX] MYPAGE 오류 수정

--- src/pages/mypage/getMypage.js
+++ src/pages/mypage/getMypage.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import Calendar from 'react-calendar';
 import 'react-calendar/dist/Calendar.css';
-import '../../styles/mypage/getMypage.css';
+import '../../styles/myPage/getMypage.css';
 import Modal from '../../components/common/Modal';
 import apiClient from '../../utils/axios';
 const GetMypage = () => {
@@ -27,7 +27,6 @@ const GetMypage = () => {
   // ✅ 추가: 활성 탭 상태
   const [activeTab, setActiveTab] = useState('ROUTINES');
   const [missions, setMissions] = useState([]);
-  const [loadingMissions, setLoadingMissions] = useState(true);
 
   // 리마인더
   const [showTimeModal, setShowTimeModal] = useState(false);
